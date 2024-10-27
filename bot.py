d38e3009555be4803b1c9aaf7a0863ca9df8932d
Elouan Grimm <elouan.grimm@gmail.com>
2024-10-27 15:10:35 +0200

    beginning tests!

--- bot.py
+++ bot.py
@@ -3,24 +3,22 @@ from discord.ext import commands
 import os
 
 intents = discord.Intents.default()
-intents.message_content = True  # Enable reading message content
-
+intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="Organizing Tabs"))
     print(f"We have logged in as {bot.user}")
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send("Hello! 👋")
+async def link(ctx):
+    user = ctx.author
+    await ctx.send(f"{user.mention} Here’s where you can install the Tidy Tab Groups extension: [hey! click me! :P](<https://chromewebstore.google.com/detail/tidy-tab-groups/fohgbkobjdckaapjimleemkolchkmebf>)")
 
 @bot.command()
-async def info(ctx):
-    await ctx.send("I'm a bot running on Heroku! 🎉")
-
-@bot.command()
-async def echo(ctx, *, message):
-    await ctx.send(message)
+async def ping(ctx):
+    latency = round(bot.latency * 1000)
+    await ctx.send(f"Pong! 🏓 *(Latency: {latency} ms)*")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
