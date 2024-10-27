2a87de94ecb4958c4f80377fe183aceae2ac5ae0
Elouan Grimm <elouan.grimm@gmail.com>
2024-10-27 15:28:14 +0200

    updates!

--- bot.py
+++ bot.py
@@ -6,23 +6,49 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# BOT VARIABLES:
+rich_presence = "Tab Organization"
+
+# Bot Setup & Rich Presence
+
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game(name="Organizing Tabs"))
+    await bot.change_presence(activity=discord.Game(name=rich_presence))
     print(f"We have logged in as {bot.user}")
 
+# Link Command
+
+@bot.command()
+async def link(ctx):
+    user = ctx.author
+    await ctx.send(f"{user.mention} Here's the link to install the Tidy Tab Groups extension: https://chromewebstore.google.com/detail/tidy-tab-groups/fohgbkobjdckaapjimleemkolchkmebf")
+
 @bot.tree.command(name="link", description="Get the link to install Tidy Tab Groups")
-async def link(interaction: discord.Interaction):
+async def slash_link(interaction: discord.Interaction):
     user = interaction.user
     await interaction.response.send_message(f"{user.mention} Here's the link to install the Tidy Tab Groups extension: https://chromewebstore.google.com/detail/tidy-tab-groups/fohgbkobjdckaapjimleemkolchkmebf")
 
+# Install Command
+
+@bot.command()
+async def install(ctx):
+    user = ctx.author
+    await ctx.send(f"{user.mention} Here’s where you can install the Tidy Tab Groups extension: [hey! click me! :P](<https://chromewebstore.google.com/detail/tidy-tab-groups/fohgbkobjdckaapjimleemkolchkmebf>) \n Then, you can click `Install` to get it and click accept. Enjoy organization! 😎😄😁")
+
 @bot.tree.command(name="install", description="Get instructions for how to install the Tidy Tab Groups extension")
-async def install(interaction: discord.Interaction):
+async def slash_install(interaction: discord.Interaction):
     user = interaction.user
     await interaction.response.send_message(f"{user.mention} Here’s where you can install the Tidy Tab Groups extension: [hey! click me! :P](<https://chromewebstore.google.com/detail/tidy-tab-groups/fohgbkobjdckaapjimleemkolchkmebf>) \n Then, you can click `Install` to get it and click accept. Enjoy organization! 😎😄😁")
 
-@bot.tree.command(name="ping", description="Check the bot's latency!")
-async def ping(interaction: discord.Interaction):
+# Ping Command
+
+@bot.command()
+async def ping(ctx):
+    latency = round(bot.latency * 1000)
+    await ctx.send(f"Pong! 🏓 *(Latency: {latency} ms)*")
+
+@bot.tree.command(name="ping", description="Check the bot's latency")
+async def slash_ping(interaction: discord.Interaction):
     latency = round(bot.latency * 1000)
     await interaction.response.send_message(f"Pong! 🏓 *(Latency: {latency} ms)*")
 
