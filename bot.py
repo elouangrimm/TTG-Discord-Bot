1f9364c5000cae0d67561a646a2e1f90222128e7
Elouan Grimm <elouan.grimm@gmail.com>
2024-10-27 15:20:10 +0200

    commands?

--- bot.py
+++ bot.py
@@ -11,14 +11,24 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="Organizing Tabs"))
     print(f"We have logged in as {bot.user}")
 
-@bot.command()
-async def link(ctx):
-    user = ctx.author
-    await ctx.send(f"{user.mention} Here’s where you can install the Tidy Tab Groups extension: [hey! click me! :P](<https://chromewebstore.google.com/detail/tidy-tab-groups/fohgbkobjdckaapjimleemkolchkmebf>)")
+@bot.tree.command(name="link", description="Get the link to install Tidy Tab Groups")
+async def link(interaction: discord.Interaction):
+    user = interaction.user
+    await interaction.response.send_message(f"{user.mention} Here's the link to install the Tidy Tab Groups extension: https://chromewebstore.google.com/detail/tidy-tab-groups/fohgbkobjdckaapjimleemkolchkmebf")
 
-@bot.command()
-async def ping(ctx):
+@bot.tree.command(name="install", description="Get instructions for how to install the Tidy Tab Groups extension")
+async def install(interaction: discord.Interaction):
+    user = interaction.user
+    await interaction.response.send_message(f"{user.mention} Here’s where you can install the Tidy Tab Groups extension: [hey! click me! :P](<https://chromewebstore.google.com/detail/tidy-tab-groups/fohgbkobjdckaapjimleemkolchkmebf>) \n Then, you can click `Install` to get it and click accept. Enjoy organization! 😎😄😁")
+
+@bot.tree.command(name="ping", description="Check the bot's latency!")
+async def ping(interaction: discord.Interaction):
     latency = round(bot.latency * 1000)
-    await ctx.send(f"Pong! 🏓 *(Latency: {latency} ms)*")
+    await interaction.response.send_message(f"Pong! 🏓 *(Latency: {latency} ms)*")
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"We have logged in as {bot.user}")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
